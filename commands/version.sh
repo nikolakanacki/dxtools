6b72040ae2f840ace050c58e30f0567726962765
Nikola Kanacki <mail@nikolakanacki.com>
2018-05-08 17:17:44 +0200

    fix: prevent version bumps on branches other than master

--- commands/version.sh
+++ commands/version.sh
@@ -3,6 +3,9 @@
 if ! [ -z "$(git status --porcelain)" ]; then
   echo 'Clean up your working directory before making a version bump';
   exit 1;
+elif [ "$(git symbolic-ref --short -q HEAD)" != 'master' ]; then
+  echo 'Cannot perform a version bump version on branches other than master';
+  exit 1;
 fi;
 
 npm --no-git-tag-version version $1;
