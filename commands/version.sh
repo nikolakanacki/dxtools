f9a3aa49fdbe5ac62c3f12bcd6be1105b599f909
Nikola Kanacki <mail@nikolakanacki.com>
2018-06-11 13:06:03 +0200

    fix: handle prereleases better

--- commands/version.sh
+++ commands/version.sh
@@ -8,19 +8,48 @@ elif [ "$(git symbolic-ref --short -q HEAD)" != 'master' ]; then
   exit 1;
 fi;
 
-if [ "$2" != '' ]; then
+argVersion="$1";
+argPreid="$2";
+
+if [ "$argPreid" != '' ]; then
+  case "$argVersion" in
+    'major')
+      argVersion='premajor';
+    ;;
+    'minor')
+      argVersion='preminor';
+    ;;
+    'patch')
+      argVersion='prepatch';
+    ;;
+  esac;
+  if ! [[ $argVersion =~ ^pre ]]; then
+    echo "=> Error: Cannot perform a prerelease version bump with \"${argVersion}\"";
+    exit 1;
+  fi;
   npm_package_new_version=$(\
     semver \
     $npm_package_version \
-    --increment $1 \
-    --preid $2\
+    --increment $argVersion \
+    --preid $argPreid \
   );
 else
-  npm_package_new_version=$(\
-    semver \
-    $npm_package_version \
-    --increment $1 \
-  );
+  case "$argVersion" in
+    'major'|'minor'|'patch')
+      npm_package_new_version=$(\
+        semver \
+        $npm_package_version \
+        --increment $argVersion \
+      );
+    ;;
+    *)
+      npm_package_new_version="$argVersion";
+    ;;
+  esac;
+fi;
+
+if [ -z "$npm_package_new_version" ]; then
+  exit 1;
 fi;
 
 npm --no-git-tag-version version $npm_package_new_version;
