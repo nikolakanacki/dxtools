18df04ab785ddff09bdbc7b388dde10ed92032e0
Nikola Kanacki <mail@nikolakanacki.com>
2020-01-23 17:41:13 +0100

    fix: ensure env files in docker command

--- commands/docker.sh
+++ commands/docker.sh
@@ -7,6 +7,21 @@ ensureComposeFiles() {
   fi;
 }
 
+ensureEnvFiles() {
+  if ! [ -f ".env.default" ]; then
+    touch ".env.default";
+  fi;
+  if ! [ -f ".env.${DXTOOLS_ENV}" ]; then
+    touch ".env.${DXTOOLS_ENV}";
+  fi;
+  if ! [ -f ".env.${DXTOOLS_ENV}.local" ]; then
+    touch ".env.${DXTOOLS_ENV}.local";
+  fi;
+  if ! [ -f ".env" ]; then
+    touch ".env";
+  fi;
+}
+
 export ID_RSA="$(cat ~/.ssh/id_rsa 2> /dev/null)";
 export ID_RSA_PUB="$(cat ~/.ssh/id_rsa.pub 2> /dev/null)";
 
@@ -31,6 +46,7 @@ while test $# -gt 0; do
           exit 0;
         ;;
         'enter'|'exec')
+          ensureEnvFiles;
           ensureComposeFiles;
           CONTAINER_NAME="${npm_package_organization}-${npm_package_name}-$1"; shift;
           CONTAINER_COMMAND=$1; shift;
@@ -41,6 +57,7 @@ while test $# -gt 0; do
           exit 0;
         ;;
         'restart')
+          ensureEnvFiles;
           ensureComposeFiles;
           RESTART_CONTAINER_NAME="${npm_package_organization}-${npm_package_name}-$1";
           shift;
@@ -228,6 +245,7 @@ EOF
           esac;
         ;;
         *)
+          ensureEnvFiles;
           ensureComposeFiles;
           docker-compose $ARG_COMMAND $@;
           exit 0;
